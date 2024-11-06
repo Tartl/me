1d38686267e643881a2e339be49653f342c86539
Tartl <91676175+Tartl@users.noreply.github.com>
2024-11-06 23:41:00 +0100

    ukol do webu :+1:

---
+++ responsive-bloky.js
@@ -0,0 +1,31 @@
+function calc(){
+    var number1 = parseInt(document.getElementById("number1").value);
+    var number2 = parseInt(document.getElementById("number2").value);
+    var operand = document.getElementById("operand").value;
+    var result = 0;
+    switch (operand) {
+        case "+":
+        {
+            result = number1 + number2;
+            break;
+        }
+        case "-":  
+        {
+            result = number1 - number2;
+            break;
+        }
+        case "*":
+        {
+            result = number1 * number2;
+            break;
+        }
+        case "/":
+        {
+            if(number2 != 0)
+                result = number1 / number2;
+            else console.log("Dividing by zero!");
+            break;
+        }
+    }
+        alert(result);
+}
