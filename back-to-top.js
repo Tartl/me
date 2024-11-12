12bd5ddd1198e7460bbd5167ff482eedbc48bef5
Tartl <91676175+Tartl@users.noreply.github.com>
2024-11-12 22:41:03 +0100

    back-to-top fix

--- back-to-top.js
+++ back-to-top.js
@@ -1,5 +1,5 @@
 let backToTopVisible = false;
-let isAnimating = false; 
+let isAnimating = false;
 
 window.addEventListener('scroll', function() {
     const backToTopButton = document.querySelector('.back-to-top');
@@ -7,23 +7,22 @@ window.addEventListener('scroll', function() {
     if (window.scrollY > 100) {
         if (!backToTopVisible && !isAnimating) {
             backToTopVisible = true;
-            backToTopButton.style.display = 'block';
+            backToTopButton.style.display = 'block'; 
             setTimeout(() => {
-                backToTopButton.style.opacity = '1';
+                backToTopButton.style.opacity = '1'; 
             }, 10);
         }
     } else {
         if (backToTopVisible && !isAnimating) {
-            isAnimating = true; 
-            backToTopButton.style.opacity = '0';
+            isAnimating = true;
+            backToTopButton.style.opacity = '0'; 
             setTimeout(() => {
-                backToTopButton.style.display = 'none';
-                backToTopVisible = false; 
-                isAnimating = false; 
+                backToTopButton.style.display = 'none'; 
+                backToTopVisible = false;
+                isAnimating = false;
             }, 300); 
         }
     }
 });
-
 
 
